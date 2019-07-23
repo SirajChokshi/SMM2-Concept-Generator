d5150e47c418804ddfd04d3e25ae3a9d6ff87848
Siraj <sirajchokshi@gmail.com>
2019-07-22 20:53:10 -0400

    fix -incompatability -filters -array sizing

--- app.js
+++ app.js
@@ -1,16 +1,49 @@
 var style = null;
 var theme = null;
 var day = true;
+var item1 = "";
+var item2 = "";
+var item3 = "";
 var styles = ["SMB", "SMB3", "SMW", "NSMB", "3DW"];
 var themes = ["Ground", "Underground", "Underwater", "Desert", "Snow", "Sky", "Forest", "Ghost House", "Airship", "Castle"];
-var common = ["Banzai Bills", "Bloopers", "Bob-ombs", "Boos", "Boom Boom", "Bowser", "Bowser Jr.", "Bullet Bills", "Buzzy Beetles", "Chain Chomps", "Cheep Cheeps", "Dry Bones", "Pirahna Plants", "Fish Bones", "Goombas", "Hammer Bros", "Koopa Troopas", "Lakitu", "Lava Bubbles", "Magikoopas", "Monty Moles", "Moon", "Munchers", "Rocket Wrenches", "Sledge Bros", "Spike Tops", "Spinies", "Stretches", "Thwomps", "Twisters", "Wiggler"];
-var exclusives_all = ["the Angry Sun", "Poisonous Mushrooms"];
+var common = ["Banzai Bills", "Bloopers", "Bob-ombs", "Boos", "Boom Boom", "Bowser", "Bullet Bills", "Buzzy Beetles", "Chain Chomps", "Cheep Cheeps", "Dry Bones", "Pirahna Plants", "Fish Bones", "Goombas", "Hammer Bros", "Koopa Troopas", "Lakitu", "Lava Bubbles", "Magikoopas", "Monty Moles", "Munchers", "Rocky Wrenches", "Sledge Bros", "Spike Tops", "Spinies", "Stretches", "Thwomps", "Twisters", "Wiggler", "Precision Platforming", ""];
+var exclusives_not_tdw = ["Bowser Jr.", "On/Off blocks"];
+var exclusives_day_ground = ["Poisonous Mushrooms"];
+var exclusives_day = ["the Angry Sun"];
+var exclusives_night = ["the Moon"];
 var exclusives_tdw = ["Ant Troopers", "Bullies", "Cat Banzai Bills", "Cat Bullet Bills", "Charvaargh", "Fire Bros", "Meowser", "Peepas", "Piranha Creepers", "Pom Pom", "Porcupuffer", "Skipsqueaks", "Stingbies", "Koopa Cars", "clear pipes", "trees"];
-var twists = ["make it vertical", "add a boss battle", "design a puzzle", "use a sub-world", "make it a speedrun", "require spin-jumps", "add custom music", "make it non-linear", "add a clear condition", "don't allow jumping", "have a true ending"];
+var twists = ["make it vertical", "add a boss battle", "design a puzzle", "use a sub-world", "make it a speedrun", "add custom music", "make it non-linear", "add a clear condition", "don't allow jumping", "have a true ending"];
+var twists_movement = ["require spin-jumps"];
+
+function reset() {
+  if (!$('#styleLock').prop('checked')) style = null;
+  if (!$('#themeLock').prop('checked')) theme = null;
+  if (!$('#timeLock').prop('checked')) day = true;
+  if (!$('#enemiesLock').prop('checked')) {
+    item1 = null;
+    item2 = null;
+    item3 = null;
+  }
+}
 
 function genStyle() {
+  reset();
   if(!$('#styleLock').prop('checked')) {
     style = styles[Math.floor(Math.random() * styles.length)];
+    /* style theme */
+    for (var i = 0; i < exclusives_tdw.length; i++) {
+      if (item1 == exclusives_tdw[i] || item2 == exclusives_tdw[i] || item3 == exclusives_tdw[i]) {
+        style = "3DW";
+        break;
+      }
+    }
+    for (var i = 0; i < exclusives_not_tdw.length; i++) {
+      if (item1 == exclusives_not_tdw[i] || item2 == exclusives_not_tdw[i] || item3 == exclusives_not_tdw[i]) {
+        var styles_without_3DW = ["SMB", "SMB3", "SMW", "NSMB"];
+        style = styles_without_3DW[Math.floor(Math.random() * styles_without_3DW.length)];
+        break;
+      }
+    }
   }
 }
 
@@ -35,16 +68,26 @@ function genAll() {
     else {
       day = false;
     }
+    if (item1 == "Moon" || item2 == "Moon" || item3 == "Moon" || item1 == "Poisonous Mushrooms" || item2 == "Poisonous Mushrooms" || item3 == "Poisonous Mushrooms") {
+      day = false;
+    }
+    if (item1 == "the Angry Sun" || item2 == "the Angry Sun" || item3 == "the Angry Sun") {
+      day = true;
+    }
   }
-  /* Gimmicks/Enemies */
+  /* Enemies/Gimmicks */
   if (!$('#enemiesLock').prop('checked')) {
-    var arrLen = common.length;
-    if (style == "3DW") arrLen += exclusives_tdw.length;
-    else arrLen += exclusives_all.length;
+    var list = common;
+    if(style == "3DW") list = list.concat(exclusives_tdw);
+    else list = list.concat(exclusives_not_tdw);
+    if(theme == "Ground" && day) list = list.concat(exclusives_day_ground);
+    if(day && style != "3DW") list = list.concat(exclusives_day);
+    else if(style != "3DW") list = list.concat(exclusives_night);
+
     var chosen = 0;
     var used = [null, null, null];
     while (chosen < 3) {
-       var nextIndex = Math.floor(Math.random() * arrLen);
+       var nextIndex = Math.floor(Math.random() * list.length);
        var usedYet = false;
        for (var k = 0; k < used.length; k++) {
          if(used[k] == nextIndex) usedYet = true;
@@ -58,31 +101,28 @@ function genAll() {
     var htmlInsert = "";
 
     for (var i = 0; i < used.length; i++) {
-      var insertVal = "";
-      if (used[i] < common.length) {
-        insertVal = common[used[i]];
-      }
-      else if (style == "3DW") {
-        insertVal = exclusives_tdw[used[i] - common.length];
-      }
-      else {
-        insertVal = exclusives_all[used[i] - common.length];
-      }
-      if(i > 0) htmlInsert += " & ";
-      htmlInsert += insertVal;
+      if (i == 0) item1 = list[used[i]];
+      if (i == 1) item2 = list[used[i]];
+      if (i == 2) item3 = list[used[i]];
+      htmlInsert += list[used[i]];
+      if(i != 2) htmlInsert += " & ";
     }
+
   }
 
   if (!$('#twistLock').prop('checked')) {
-    var twist = twists[Math.floor(Math.random() * twists.length)];
+    var tempTwists = twists;
+    if (style != "SMB" && style != "SMB3") tempTwists = tempTwists.concat(twists_movement);
+    var tempTwist = tempTwists[Math.floor(Math.random() * tempTwists.length)];
   }
 
-  var delay = 800 + Math.random() * 1000;
+// TOODO add delay 800 & 1000 from 0
+  var delay = 0 + Math.random() * 0;
 
   setTimeout(function(){
     $('#generated').css('opacity', '1');
     $('.lds-roller').css('display', 'none');
-    update(htmlInsert, twist);
+    update(htmlInsert, tempTwist);
   }, delay);
 }
 
